6e224a0a94da31034701b8fd79be3eb8bf95b622
orpeq <to.antanaitis@gmail.com>
2023-06-18 11:36:54 +0300

    fix-i18n-issue
    Fixed issue/typo for not calling i18n function when exporting App component.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import "@styles/globals.css"
-import { appTheme } from "@styles/systemDesign"
 import type { AppProps } from "next/app"
+import { appWithTranslation } from "next-i18next"
 import { ThemeProvider } from "styled-components"
 
-export default function App({ Component, pageProps }: AppProps) {
+import { appTheme } from "@styles/systemDesign"
+
+function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={appTheme}>
       <Component {...pageProps} />
     </ThemeProvider>
   )
 }
+
+export default appWithTranslation(App)
