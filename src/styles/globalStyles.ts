b8144072032fdd654e7456c9b75d23df5acf1cb1
TomasAnt <hi@atomas.io>
2023-07-28 12:32:26 +0300

    design-system-and-units-changes

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,11 +1,18 @@
 import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyles = createGlobalStyle`
+
+html {
+  font-size: 100%; //1 rem = 16px
+  margin: 0;
+}
+
 html,
 body {
   padding: 0;
   margin: 0;
   overflow-x: hidden;
+  background-color: ${({ theme }) => theme.colors.primary.background};
 }
 
 body,
@@ -23,5 +30,9 @@ a {
 
 * {
   box-sizing: border-box;
+}
+
+p, h1, h2, h3, h4, h5, h6 {
+  margin: 0;
 }
 `
